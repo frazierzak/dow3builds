3728806e15a8f5e2d8d2660b08b6476369f527ee
Zak Frazier <zakfrazier@gmail.com>
2017-05-10 13:26:32 -0400

    New logo, testing number queries

--- main.js
+++ main.js
@@ -23,6 +23,28 @@ var thirdEliteDocName;
 
 var testing = true;
 
+if (testing) {
+    var user = "tester";
+}
+
+//Get Number of Builds User has made
+var ref = firebase.database().ref("users/" + user);
+ref.once("value")
+  .then(function(snapshot) {
+    var a = snapshot.child("builds").numChildren();
+    var b = snapshot.child("favs").numChildren();
+    $("#yourBuildNumber").text(a);
+    $("#yourFavNumber").text(b);
+  });
+
+var ref = firebase.database().ref("builds");
+ref.once("value")
+  .then(function(snapshot) {
+    var a = snapshot.numChildren();
+    $("#allBuildsNumber").text(a);
+  });
+
+
 //Clear code
 function clear() {
     $("#form input").removeAttr("checked");
